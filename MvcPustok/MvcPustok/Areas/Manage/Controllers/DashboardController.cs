fff1a5dcbf196a97885d9c9f273ad0f601bd42bd
ElmarQarayev1 <elmareg@code.edu.az>
2024-05-13 23:53:46 +0400

    folder changed

---
+++ MvcPustok/MvcPustok/Areas/Manage/Controllers/DashboardController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MvcPustok.Data;
+
+namespace MvcPustok.Areas.Manage.Controllers
+{
+    [Authorize(Roles = "admin,superadmin")]
+    [Area("manage")]
+    public class DashboardController:Controller
+	{
+	
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
